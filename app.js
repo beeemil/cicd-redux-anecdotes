96f1beace43ebd61cbcb3a3193b3fa7bcff4db20
beeemil <eemil.becker@helsinki.fi>
2024-09-15 19:23:58 +0300

    fixing

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const path = require('path');
 
 const app = express()
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/api', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.get('/version', (_req, res) => {
   res.send('5')
