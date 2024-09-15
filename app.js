6ad4c62ab0fb991913658e69b5d192d10cc771c2
beeemil <eemil.becker@helsinki.fi>
2024-09-15 19:57:37 +0300

    fixing

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/api', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.get('/version', (_req, res) => {
