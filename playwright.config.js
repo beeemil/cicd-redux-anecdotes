400bdb487716a25555a9e6b0c81164a8698d72c3
beeemil <eemil.becker@helsinki.fi>
2024-09-15 18:17:59 +0300

    test

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test'
 export default defineConfig({
   testDir: './e2e-tests',
   webServer: {
-    command: 'npm run dev && npm run server',
+    command: 'npm run start',
     url: 'http://127.0.0.1:4173',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
